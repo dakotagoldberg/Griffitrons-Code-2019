5cd9d98998545d9a64d449e44048e20446b385c7
Dakota Goldberg <dakotaelle@icloud.com>
2019-01-13 15:52:50 -0800

    changed stuff.

--- src/main/java/frc/robot/ControlScheme2.java
+++ src/main/java/frc/robot/ControlScheme2.java
@@ -6,7 +6,6 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.Joystick;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
-import edu.wpi.first.wpilibj.drive.MecanumDrive;
 
 
 
@@ -32,9 +31,6 @@ public class ControlScheme2 extends TimedRobot {
     @Override
     public void teleopPeriodic() {
 
-        
-
-        
     }
 
 
