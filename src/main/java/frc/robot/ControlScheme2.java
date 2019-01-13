e227cd8bf5473ea14ecd475b46785c10aeeaf620
lkakinuma <lkakinuma@buckley.org>
2019-01-13 15:13:21 -0800

    Moving

--- src/main/java/frc/robot/ControlScheme2.java
+++ src/main/java/frc/robot/ControlScheme2.java
@@ -2,29 +2,27 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 
 import edu.wpi.first.wpilibj.Joystick;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.Talon;
 
 
 
 
-import com.ctre.phoenix.motorcontrol.can.*;
-import edu.wpi.first.wpilibj.*;
-import edu.wpi.first.wpilibj.drive.*;
 
 public class ControlScheme2 extends TimedRobot {
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
