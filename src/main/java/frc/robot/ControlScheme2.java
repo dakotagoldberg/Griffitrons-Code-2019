194b2509f319e0251c02bf2a00d5a2cee4d1d947
Dakota Goldberg <dakotaelle@icloud.com>
2019-01-13 15:16:40 -0800

    Added a teleopinit.

--- src/main/java/frc/robot/ControlScheme2.java
+++ src/main/java/frc/robot/ControlScheme2.java
@@ -1,6 +1,8 @@
 package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Talon;
 
@@ -14,6 +16,11 @@ public class ControlScheme2 extends TimedRobot {
     WPI_TalonSRX frontL = new WPI_TalonSRX(deviceNumber);
     WPI_TalonSRX backR = new WPI_TalonSRX(deviceNumber);
     WPI_TalonSRX backL = new WPI_TalonSRX(deviceNumber);
+
+    @Override
+    public void teleopInit() {
+
+    }
 
 
 
