ee3635faa40efd8da3d7c0fd3a14f2b9e4275dcf
LAPTOP-3PN2HQTI\Spenc <spencergcollins@gmail.com>
2019-01-13 15:11:11 -0800

    no merge plz import

--- src/main/java/frc/robot/ControlScheme2.java
+++ src/main/java/frc/robot/ControlScheme2.java
@@ -2,7 +2,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Talon;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 
 
