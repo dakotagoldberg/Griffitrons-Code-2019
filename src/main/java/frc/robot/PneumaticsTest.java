9c96fbc8a83d27d8e3501b83a5152fc891f5af48
CarneAsadaFry <nateaugust11@gmail.com>
2019-01-29 17:34:50 -0800

    Updated PneumaticsTest.java

--- src/main/java/frc/robot/PneumaticsTest.java
+++ src/main/java/frc/robot/PneumaticsTest.java
@@ -6,12 +6,39 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 /**
  * Add your docs here.
  */
 public class PneumaticsTest extends TimedRobot {
+    DoubleSolenoid sole = new DoubleSolenoid(3, 2);
+    Joystick joy = new Joystick(0);
+	WPI_TalonSRX bLeft = new WPI_TalonSRX(1);
 
+    @Override
+    public void teleopInit() {
+
+    }
+
+    @Override
+    public void teleopPeriodic() {
+        double y = -joy.getRawAxis(1);
+        if(Math.abs(y) > 0.05)
+            bLeft.set(y);
+        else 
+            bLeft.set(0);
+
+        if(joy.getRawButton(3) && bLeft.get() > 0.05)
+            sole.set(DoubleSolenoid.Value.kReverse);
+        else if(joy.getRawButton(4) && bLeft.get() > 0.05)
+            sole.set(DoubleSolenoid.Value.kForward);
+        else
+            sole.set(DoubleSolenoid.Value.kOff);
+    }
 
 }
