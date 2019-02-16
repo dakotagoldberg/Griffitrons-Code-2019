5c18ac6278313540341d1b328f44ad25917d8a68
LAPTOP-3PN2HQTI\Spenc <spencergcollins@gmail.com>
2019-02-16 12:40:24 -0800

    changes

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,46 +11,48 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 public class Robot extends TimedRobot implements Drive_Constants, Control_Constants {
     double x, y, throttle, turn, speedL, speedR;
 
-    WPI_TalonSRX fLeft = new WPI_TalonSRX(front_left);
-    WPI_TalonSRX mLeft = new WPI_TalonSRX(middle_left);
-    WPI_TalonSRX bLeft = new WPI_TalonSRX(back_left);
-    WPI_TalonSRX fRight = new WPI_TalonSRX(front_right);
-    WPI_TalonSRX mRight = new WPI_TalonSRX(middle_right);
-    WPI_TalonSRX bRight = new WPI_TalonSRX(back_right);
+    // WPI_TalonSRX fLeft = new WPI_TalonSRX(front_left);
+    // WPI_TalonSRX mLeft = new WPI_TalonSRX(middle_left);
+    // WPI_TalonSRX bLeft = new WPI_TalonSRX(back_left);
+    // WPI_TalonSRX fRight = new WPI_TalonSRX(front_right);
+    // WPI_TalonSRX mRight = new WPI_TalonSRX(middle_right);
+    // WPI_TalonSRX bRight = new WPI_TalonSRX(back_right);
 
-    EchoServer jetson;
+    // EchoServer jetson;
+    Intake claws;
 
-    SpeedControllerGroup left = new SpeedControllerGroup(fLeft, mLeft, bLeft);
-    SpeedControllerGroup right = new SpeedControllerGroup(fRight, mRight, bRight);
+    // SpeedControllerGroup left = new SpeedControllerGroup(fLeft, mLeft, bLeft);
+    // SpeedControllerGroup right = new SpeedControllerGroup(fRight, mRight, bRight);
 
-    DifferentialDrive TestCoast = new DifferentialDrive(left, right);
+    // DifferentialDrive TestCoast = new DifferentialDrive(left, right);
     XboxController driveBox = new XboxController(drive_controller);
 
-    DoubleSolenoid leftSole = new DoubleSolenoid(0, 1);
-    DoubleSolenoid rightSole = new DoubleSolenoid(2, 3);
+    // DoubleSolenoid leftSole = new DoubleSolenoid(0, 1);
+    // DoubleSolenoid rightSole = new DoubleSolenoid(2, 3);
 
     @Override
     public void robotInit() {
-        jetson = new EchoServer();
+        // jetson = new EchoServer();
+        claws = new Intake();
 
-        fLeft.configContinuousCurrentLimit(continuous_current);
-        mLeft.configContinuousCurrentLimit(continuous_current);
-        bLeft.configContinuousCurrentLimit(continuous_current);
-        fRight.configContinuousCurrentLimit(continuous_current);
-        mRight.configContinuousCurrentLimit(continuous_current);
-        bRight.configContinuousCurrentLimit(continuous_current);
-        fLeft.configPeakCurrentLimit(peak_current);
-        mLeft.configPeakCurrentLimit(peak_current);
-        bLeft.configPeakCurrentLimit(peak_current);
-        fRight.configPeakCurrentLimit(peak_current);
-        mRight.configPeakCurrentLimit(peak_current);
-        bRight.configPeakCurrentLimit(peak_current);
-        fLeft.configOpenloopRamp(open_ramp);
-        mLeft.configOpenloopRamp(open_ramp);
-        bLeft.configOpenloopRamp(open_ramp);
-        fRight.configOpenloopRamp(open_ramp);
-        mRight.configOpenloopRamp(open_ramp);
-        bRight.configOpenloopRamp(open_ramp);
+        // fLeft.configContinuousCurrentLimit(continuous_current);
+        // mLeft.configContinuousCurrentLimit(continuous_current);
+        // bLeft.configContinuousCurrentLimit(continuous_current);
+        // fRight.configContinuousCurrentLimit(continuous_current);
+        // mRight.configContinuousCurrentLimit(continuous_current);
+        // bRight.configContinuousCurrentLimit(continuous_current);
+        // fLeft.configPeakCurrentLimit(peak_current);
+        // mLeft.configPeakCurrentLimit(peak_current);
+        // bLeft.configPeakCurrentLimit(peak_current);
+        // fRight.configPeakCurrentLimit(peak_current);
+        // mRight.configPeakCurrentLimit(peak_current);
+        // bRight.configPeakCurrentLimit(peak_current);
+        // fLeft.configOpenloopRamp(open_ramp);
+        // mLeft.configOpenloopRamp(open_ramp);
+        // bLeft.configOpenloopRamp(open_ramp);
+        // fRight.configOpenloopRamp(open_ramp);
+        // mRight.configOpenloopRamp(open_ramp);
+        // bRight.configOpenloopRamp(open_ramp);
     }
 
     @Override
@@ -60,16 +62,16 @@ public class Robot extends TimedRobot implements Drive_Constants, Control_Consta
 
     @Override
     public void autonomousInit() {
-        jetson.start();
+       // jetson.start();
     }
 
     @Override
     public void autonomousPeriodic() {
-        throttle = jetson.getThrottle();
-        speedL = throttle;
-        speedR = throttle;
-        // System.out.println(speedL + " " + speedR);
-        TestCoast.tankDrive(speedL, speedR);
+        // throttle = jetson.getThrottle();
+        // speedL = throttle;
+        // speedR = throttle;
+        // // System.out.println(speedL + " " + speedR);
+        // TestCoast.tankDrive(speedL, speedR);
     }
 
     @Override
@@ -79,25 +81,25 @@ public class Robot extends TimedRobot implements Drive_Constants, Control_Consta
 
     @Override
     public void teleopPeriodic() {
-        y = -driveBox.getRawAxis(left_y_axis);
-        x = driveBox.getRawAxis(right_x_axis);
+        // y = -driveBox.getRawAxis(left_y_axis);
+        // x = driveBox.getRawAxis(right_x_axis);
 
-        if (Math.abs(y) > 0.01)
-            throttle = y;
-        else
-            throttle = 0.0;
+        // if (Math.abs(y) > 0.01)
+        //     throttle = y;
+        // else
+        //     throttle = 0.0;
 
-        if (Math.abs(x) > 0.25)
-            turn = x;
-        else
-            turn = 0.0;
+        // if (Math.abs(x) > 0.25)
+        //     turn = x;
+        // else
+        //     turn = 0.0;
 
-        // if(Math.abs(x) > 0.05 && Math.abs(y) > 0.05){
-        /*
-         * if(y <= 0.1 && y >= -0.1){ speedL = x; speedR = -x; } else { if(x > 0) speedL
-         * = y; speedR = y * (1-(x*3/4)); }else{ speedR = y; speedL = y *
-         * (1-(Math.abs(x)*3/4)); } }
-         */
+        // // if(Math.abs(x) > 0.05 && Math.abs(y) > 0.05){
+        // /*
+        //  * if(y <= 0.1 && y >= -0.1){ speedL = x; speedR = -x; } else { if(x > 0) speedL
+        //  * = y; speedR = y * (1-(x*3/4)); }else{ speedR = y; speedL = y *
+        //  * (1-(Math.abs(x)*3/4)); } }
+        //  */
         // Making turns at high speed possible
         // Need to figure out the gain value for turning
         // But when throttle = 0, the robot won't turn.
@@ -110,25 +112,27 @@ public class Robot extends TimedRobot implements Drive_Constants, Control_Consta
         // if(throttle > 0.5)
         // turn = turn * (gain_turn * Math.abs(throttle));
 
-        double t_left, t_right;
-        t_left = throttle + turn;
-        t_right = throttle - turn;
+        // double t_left, t_right;
+        // t_left = throttle + turn;
+        // t_right = throttle - turn;
 
-        speedL = t_left + skim(t_right);
-        speedR = t_right + skim(t_left);
+        // speedL = t_left + skim(t_right);
+        // speedR = t_right + skim(t_left);
 
-        TestCoast.tankDrive(speedL, speedR);
+        // TestCoast.tankDrive(speedL, speedR);
 
-        if (driveBox.getRawButton(left_bumper)) {
-            leftSole.set(DoubleSolenoid.Value.kReverse);
-            rightSole.set(DoubleSolenoid.Value.kReverse);
-        } else if (driveBox.getRawButton(right_bumper)) {
-            leftSole.set(DoubleSolenoid.Value.kForward);
-            rightSole.set(DoubleSolenoid.Value.kForward);
-        } else {
-            leftSole.set(DoubleSolenoid.Value.kOff);
-            rightSole.set(DoubleSolenoid.Value.kOff);
-        }
+        // if (driveBox.getRawButton(left_bumper)) {
+        //     leftSole.set(DoubleSolenoid.Value.kReverse);
+        //     rightSole.set(DoubleSolenoid.Value.kReverse);
+        // } else if (driveBox.getRawButton(right_bumper)) {
+        //     leftSole.set(DoubleSolenoid.Value.kForward);
+        //     rightSole.set(DoubleSolenoid.Value.kForward);
+        // } else {
+        //     leftSole.set(DoubleSolenoid.Value.kOff);
+        //     rightSole.set(DoubleSolenoid.Value.kOff);
+        // }
+
+       
 
     }
 
@@ -139,59 +143,64 @@ public class Robot extends TimedRobot implements Drive_Constants, Control_Consta
 
     @Override
     public void testPeriodic() {
-        if (driveBox.getRawButton(left_bumper)) {
-            fLeft.set(0);
-            mLeft.set(0);
-            mLeft.set(0);
-            if (driveBox.getRawButton(x_button)) {
-                fRight.set(.5);
-                mRight.set(0);
-                bRight.set(0);
-            } else if (driveBox.getRawButton(y_button)) {
-                fRight.set(0);
-                mRight.set(.5);
-                bRight.set(0);
-            } else if (driveBox.getRawButton(b_button)) {
-                fRight.set(0);
-                mRight.set(0);
-                bRight.set(.5);
-            } else {
-                fRight.set(0);
-                mRight.set(0);
-                bRight.set(0);
-            }
-        } else {
-            fRight.set(0);
-            mRight.set(0);
-            bRight.set(0);
-            if (driveBox.getRawButton(x_button)) {
-                fLeft.set(.5);
-                mLeft.set(0);
-                bLeft.set(0);
-            } else if (driveBox.getRawButton(y_button)) {
-                fLeft.set(0);
-                mLeft.set(.5);
-                bLeft.set(0);
-            } else if (driveBox.getRawButton(b_button)) {
-                fLeft.set(0);
-                mLeft.set(0);
-                bLeft.set(.5);
-            } else {
-                fLeft.set(0);
-                mLeft.set(0);
-                bLeft.set(0);
-            }
-        }
-    }
 
-    public double skim(double v) {
-        if (v > 1.0)
-            return -((v - 1.0) * gain_skim);
+    if(driveBox.getRawButton(4))
+        claws.hatchIntake();
+    else if(driveBox.getRawButton(5))
+        claws.hatchShoot();
+    //     if (driveBox.getRawButton(left_bumper)) {
+    //         fLeft.set(0);
+    //         mLeft.set(0);
+    //         mLeft.set(0);
+    //         if (driveBox.getRawButton(x_button)) {
+    //             fRight.set(.5);
+    //             mRight.set(0);
+    //             bRight.set(0);
+    //         } else if (driveBox.getRawButton(y_button)) {
+    //             fRight.set(0);
+    //             mRight.set(.5);
+    //             bRight.set(0);
+    //         } else if (driveBox.getRawButton(b_button)) {
+    //             fRight.set(0);
+    //             mRight.set(0);
+    //             bRight.set(.5);
+    //         } else {
+    //             fRight.set(0);
+    //             mRight.set(0);
+    //             bRight.set(0);
+    //         }
+    //     } else {
+    //         fRight.set(0);
+    //         mRight.set(0);
+    //         bRight.set(0);
+    //         if (driveBox.getRawButton(x_button)) {
+    //             fLeft.set(.5);
+    //             mLeft.set(0);
+    //             bLeft.set(0);
+    //         } else if (driveBox.getRawButton(y_button)) {
+    //             fLeft.set(0);
+    //             mLeft.set(.5);
+    //             bLeft.set(0);
+    //         } else if (driveBox.getRawButton(b_button)) {
+    //             fLeft.set(0);
+    //             mLeft.set(0);
+    //             bLeft.set(.5);
+    //         } else {
+    //             fLeft.set(0);
+    //             mLeft.set(0);
+    //             bLeft.set(0);
+    //         }
+    //     }
+    // }
 
-        else if (v < -1.0)
-            return -((v + 1.0) * gain_skim);
+    // public double skim(double v) {
+    //     if (v > 1.0)
+    //         return -((v - 1.0) * gain_skim);
 
-        return 0;
+    //     else if (v < -1.0)
+    //         return -((v + 1.0) * gain_skim);
+
+    //     return 0;
     }
 
     @Override
--- src/main/java/frc/robot/auto/Intake.java
+++ src/main/java/frc/robot/auto/Intake.java
@@ -17,16 +17,17 @@ import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
  */
 public class Intake {
 
-    private WPI_TalonSRX c1 = new WPI_TalonSRX(10);
-    private WPI_TalonSRX c2 = new WPI_TalonSRX(11);
+    private TalonSRX c1 = new TalonSRX(2);
+    private TalonSRX c2 = new TalonSRX(1);
     // private WPI_TalonSRX w1 = new WPI_TalonSRX(12);
     // private WPI_TalonSRX w2 = new WPI_TalonSRX(13);
     // private WPI_TalonSRX rotation = new WPI_TalonSRX(14);
     private double h1zero = 42, h2zero = 42;
-    private double clawClosed = 42, clawOpened = 42;
+    private double clawClosed = 720, clawOpened = 720;
+    private double gearRatio = 1/100, degreesToTicks = 4096/360;
 
     private static final int kPIDLoopIdx = 0;
-    private static final int kTimeoutMs = 30;
+    private static final int kTimeoutMs = 0;
     private static final int kSlotIdx = 0;
 
     private static final double kp = 0.2, ki = 0, kd = 0, kf = 0.2, izone = 0, peakOutput = 1;
@@ -41,14 +42,18 @@ public class Intake {
 
     public void hatchIntake() {
         // turns rolllers and such to intake hatch
-        c1.set(ControlMode.MotionMagic,clawOpened);
-        c2.set(ControlMode.MotionMagic,-clawOpened);
+        System.out.println(FeedbackDevice.CTRE_MagEncoder_Absolute;
+        System.out.println(clawOpened*degreesToTicks*gearRatio);
+        //System.out.println(-clawOpened*degreesToTicks*gearRatio);
+        c1.set(ControlMode.MotionMagic,clawOpened*degreesToTicks*gearRatio);
+        c2.set(ControlMode.MotionMagic,-clawOpened*degreesToTicks*gearRatio);
     }
 
     public void hatchShoot() {
         // turns rolllers and such to release hatch
-        c1.set(ControlMode.MotionMagic,clawClosed);
-        c2.set(ControlMode.MotionMagic,-clawClosed);
+        c1.set(ControlMode.MotionMagic,clawClosed*degreesToTicks*gearRatio);
+        c2.set(ControlMode.MotionMagic,-clawClosed*degreesToTicks*gearRatio);
+        
     }
 
     public void zeroRotation() {
@@ -71,44 +76,44 @@ public class Intake {
         c1.setInverted(false);
         c2.setInverted(false);
 
-        c1.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0,10,kTimeoutMs);
-        c2.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0,10,kTimeoutMs);
-        c1.setStatusFramePeriod(StatusFrameEnhanced.Status_10_MotionMagic,10,kTimeoutMs);
-        c2.setStatusFramePeriod(StatusFrameEnhanced.Status_10_MotionMagic,10,kTimeoutMs);
+        c1.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0, 10, kTimeoutMs);
+        c2.setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0, 10, kTimeoutMs);
+        c1.setStatusFramePeriod(StatusFrameEnhanced.Status_10_MotionMagic, 10, kTimeoutMs);
+        c2.setStatusFramePeriod(StatusFrameEnhanced.Status_10_MotionMagic, 10, kTimeoutMs);
         
-        c1.configNominalOutputForward(0,kTimeoutMs);
-        c2.configNominalOutputForward(0,kTimeoutMs);
+        c1.configNominalOutputForward(0, kTimeoutMs);
+        c2.configNominalOutputForward(0, kTimeoutMs);
 
-        c1.configNominalOutputReverse(0,kTimeoutMs);
-        c2.configNominalOutputReverse(0,kTimeoutMs);
+        c1.configNominalOutputReverse(0, kTimeoutMs);
+        c2.configNominalOutputReverse(0, kTimeoutMs);
 
-        c1.configPeakOutputForward(1,kTimeoutMs);
-        c2.configPeakOutputForward(1,kTimeoutMs);
+        c1.configPeakOutputForward(1, kTimeoutMs);
+        c2.configPeakOutputForward(1, kTimeoutMs);
 
-        c1.configPeakOutputReverse(-1,kTimeoutMs);
-        c2.configPeakOutputReverse(-1,kTimeoutMs);
+        c1.configPeakOutputReverse(-1, kTimeoutMs);
+        c2.configPeakOutputReverse(-1, kTimeoutMs);
 
         c1.selectProfileSlot(kSlotIdx, kPIDLoopIdx);
         c2.selectProfileSlot(kSlotIdx, kPIDLoopIdx);
 
-        c1.config_kF(kSlotIdx,kf,kTimeoutMs);
-        c2.config_kF(kSlotIdx,kf,kTimeoutMs);
+        c1.config_kF(kSlotIdx, kf, kTimeoutMs);
+        c2.config_kF(kSlotIdx, kf, kTimeoutMs);
 
-        c1.config_kP(kSlotIdx,kp,kTimeoutMs);
-        c2.config_kP(kSlotIdx,kp,kTimeoutMs);
+        c1.config_kP(kSlotIdx, kp, kTimeoutMs);
+        c2.config_kP(kSlotIdx, kp, kTimeoutMs);
 
-        c1.config_kI(kSlotIdx,ki,kTimeoutMs);
-        c2.config_kI(kSlotIdx,ki,kTimeoutMs);
+        c1.config_kI(kSlotIdx, ki, kTimeoutMs);
+        c2.config_kI(kSlotIdx, ki, kTimeoutMs);
 
-        c1.config_kD(kSlotIdx,kd,kTimeoutMs);
-        c2.config_kD(kSlotIdx,kd,kTimeoutMs);
+        c1.config_kD(kSlotIdx, kd, kTimeoutMs);
+        c2.config_kD(kSlotIdx, kd, kTimeoutMs);
 
         c1.configMotionCruiseVelocity(15000, kTimeoutMs);
         c2.configMotionCruiseVelocity(15000, kTimeoutMs);
         c1.configMotionAcceleration(6000, kTimeoutMs);
         c2.configMotionAcceleration(6000, kTimeoutMs);
 
-        c1.setSelectedSensorPosition(0, kPIDLoopIdx,kTimeoutMs);
-        c2.setSelectedSensorPosition(0, kPIDLoopIdx,kTimeoutMs);
+        c1.setSelectedSensorPosition(0, kPIDLoopIdx, kTimeoutMs);
+        c2.setSelectedSensorPosition(0, kPIDLoopIdx, kTimeoutMs);
     }
 }
